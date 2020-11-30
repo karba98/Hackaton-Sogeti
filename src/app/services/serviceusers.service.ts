de8859754ee590eea26d148908613f9c4c88361a
karba98 <49042638+karba98@users.noreply.github.com>
2020-11-30 19:05:38 +0100

    user y pass

--- src/app/services/serviceusers.service.ts
+++ src/app/services/serviceusers.service.ts
@@ -8,14 +8,18 @@ import {HttpHeaders} from '@angular/common/http';
 @Injectable()
 export class ServiceUsers {
 
+    private token:string;
     private url: string;
     constructor(private _httpService:HttpClient){
         this.url=Global.url;
+        this.token="";
     }
 
-    login ():Observable <any>{
+    login (user, password):Observable <any>{
         let request = "/authenticate";
-        let headers = new HttpHeaders().set("Content-Type", "application/json");
+        let headers = new HttpHeaders();
+        headers.set("password",password);
+        headers.set("user",user);
         return this._httpService.post(this.url+request,{headers:headers});
     }
-}
+}s
--- src/global/global.ts
+++ src/global/global.ts
@@ -1,4 +1,3 @@
 export var Global ={
     url:'http://13.93.70.220:30120/S2VAPI',
-    token:""
 }
