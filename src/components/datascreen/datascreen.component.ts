e15c15337cdba651547ab15aea31b7451ff78dc2
karba98 <49042638+karba98@users.noreply.github.com>
2020-12-03 20:06:24 +0100

    Updated search bar

--- src/components/datascreen/datascreen.component.ts
+++ src/components/datascreen/datascreen.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 import { fileURLToPath } from 'url';
 
 @Component({
@@ -8,17 +8,29 @@ import { fileURLToPath } from 'url';
 })
 export class DatascreenComponent implements OnInit {
 
+  @ViewChild('name') name :ElementRef;
+  @ViewChild('firstname') firstname :ElementRef;
+  @ViewChild('secondname') secondname :ElementRef;
   public empleados : Array<Number>
   authenticated:boolean
   constructor() { 
     this.authenticated=true;
     this.empleados = new Array<Number>();
+    this.name = ElementRef.prototype;
+    this.firstname = ElementRef.prototype;
+    this.secondname = ElementRef.prototype;
   }
 
   ngOnInit(): void {
     for(let i=0;i<3;i++){
       this.empleados.push(1)
     }
+  }
+  search(){
+    console.log(this.name.nativeElement.value);
+    console.log(this.firstname.nativeElement.value);
+    console.log(this.secondname.nativeElement.value);
+
   }
 
 }
